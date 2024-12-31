ce3fecf083509f2e5800958b114e99e8983ceee0
davodhiambo <jesseomolo@yahoo.com>
2024-12-31 06:52:01 +0300

    feat: extract car listing info from html tags.

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"log"
+	"my-web-scraper/models"
 	"my-web-scraper/store"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
+	var listings []models.CarListing
 	response, err := http.Get("https://jiji.co.ke/cars")
-	var title string
 
 	if err != nil {
 		log.Fatal(err)
@@ -28,8 +30,15 @@ func main() {
 	}
 
 	doc.Find(".b-list-advert-base__data").Each(func(i int, s *goquery.Selection) {
-		title = s.Text()
-        records = append(records, title)
+		listing := models.CarListing{}
+
+		// Extract title, Price, Description, and Location
+		listing.Title = strings.TrimSpace(s.Find("b-advert-title-inner qa-advert-title b-advert-title-inner--div").Text())
+		listing.Price = strings.TrimSpace(s.Find(".qa-advert-price").Text())
+		listing.Description = strings.TrimSpace(s.Find(".b-list-advert-base__description-text").Text())
+		listing.Location = strings.TrimSpace(s.Find(".b-list-advert__region__text").Text())
+
+		listings = append(listings, listing)
 	})
 	
 	store.SaveToJSON(records)
