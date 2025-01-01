6eb1e1295c0ddb4568d4a006b83ed7217e53ac12
davodhiambo <jesseomolo@yahoo.com>
2025-01-01 04:06:26 +0300

    refactor: impliment ScrapeData in main.go.

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"log"
-	"strings"
-	"time"
 
-	"my-web-scraper/models"
 	"my-web-scraper/services"
 	"my-web-scraper/store"
-
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
 )
 
 func main() {
+	// Define key variables; target url and target div className
 	path := "https://jiji.co.ke/mombasa-cbd/buses"
 	targetDiv := ".b-list-advert-base__data__inner"
-	
+
+	// Scrape data from web page, handle errors if encountered
+	listings, err := services.ScrapeData(path, targetDiv)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write scraped data to JSON file
 	store.SaveToJSON(listings)
 }
