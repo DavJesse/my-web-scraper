39bee85be2bba927c53151e9774b955b1afd6120
davodhiambo <jesseomolo@yahoo.com>
2025-01-01 04:53:11 +0300

    files: add services/scrape.go. refactor: refactor most of main.go logic into ScrapeData.

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"log"
-	"my-web-scraper/models"
-	"my-web-scraper/store"
-	"net/http"
-	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"my-web-scraper/services"
+	"my-web-scraper/store"
 )
 
 func main() {
-	var listings []models.CarListing
-	path := "https://jiji.co.ke/cars"
+	// Define the URL and target div for scraping
+	path := "https://jiji.co.ke/nairobi/cars"
 	targetDiv := ".b-list-advert-base__data__inner"
-	
-	
+
+	// Scrape car listings, handle necessary errors
+	listings, err := services.ScrapeData(path, targetDiv)
+	if err != nil {
+		log.Fatalf("Failed to scrape data from %s: %v.", path, err)
+	}
+
+	// Write scraped data to JSON file
 	store.SaveToJSON(listings)
 }
